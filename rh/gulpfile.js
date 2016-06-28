7f698b3820071dc49beee5de37e5913d692c98ff
Israel Matos <israeldesigner@icloud.com>
2016-06-27 23:09:25 -0300

    subindo banco seo_site

---
+++ rh/gulpfile.js
@@ -0,0 +1,37 @@
+var gulp = require('gulp'),
+    minifycss = require('gulp-minify-css'),
+    less = require('gulp-less'),
+    sass  = require('gulp-sass'),
+    //refresh   = require('gulp-livereload'),
+    server    = require('tiny-lr')();
+
+    //livereload({ start: true })
+
+gulp.task('sass', function() {
+  gulp.src('sass/*.scss')
+  .pipe(sass())
+  .pipe(minifycss())
+  .pipe(gulp.dest('css'))
+  //.pipe(refresh(server));
+
+})
+
+gulp.task('less', function () {
+  gulp.src('less/*.less')
+  .pipe(less())
+  .pipe(minifycss())
+  .pipe(gulp.dest('css'))
+})
+
+gulp.task('watch',function(){
+  //refresh.listen();
+  gulp.watch('less/*.less',['less']);
+  gulp.watch('sass/*.scss',['sass']);
+
+});
+
+gulp.task('default',['less','sass']);
+
+/*gulp.task('sass:watch', function(){
+    gulp.watch('sass/*.sass',['sass']);
+})*/
